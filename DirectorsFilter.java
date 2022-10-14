8d38270102bbf8bbdb199c48020be821641e011e
vijayusj <kinneravijay9@gmail.com>
2022-10-14 09:21:46 +0530

    exp

---
+++ DirectorsFilter.java
@@ -0,0 +1,29 @@
+
+public class DirectorsFilter implements Filter {
+    private String myDirectors;
+    
+    public DirectorsFilter(String directors) {
+        myDirectors = directors;
+    }
+
+    //kk
+    
+    @Override
+    public boolean satisfies(String id) {
+        String[] directors = myDirectors.split(",");
+        
+        for (int k=0; k < directors.length; k++){
+            if (MovieDatabase.getDirector(id).indexOf(directors[k]) != -1){
+                return true;
+            }
+        }
+        return false;
+        
+        /*int idx = MovieDatabase.getDirector(id).indexOf(myDirectors);
+        if (idx != -1){
+            return true;
+        } else {
+            return false;
+        }*/
+    }
+}
